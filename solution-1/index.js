fb0633286d776b0b1c242f0cc505706f561d76fa
victor <victor.murithi@student.moringaschool.com>
2023-09-21 22:20:24 +0300

    completed solution1

---
+++ solution-1/index.js
@@ -0,0 +1,42 @@
+function generateGrade(studentInput){ //function that grades the students 
+
+// conditional statements to grade the studentMarks
+    if (studentInput > 79){
+        console.log("grade A");
+        return "A";
+    }
+    else if(studentInput>=60 && studentInput<=79){
+        console.log ("grade B");
+        return "B";
+    }
+    else if (studentInput>=49 && studentInput<=59){
+        console.log ("grade C");
+        return "C";
+    }
+    else if (studentInput>=40 && studentInput<=49){
+        console.log ("grade D");
+        return "D" ;
+    }
+    else if(studentInput>=0 && studentInput<=39){
+        console.log ("grade E");
+        return "E" ;
+    }
+}
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question("Please input the student's marks: ", function (userInput) {
+
+const studentInput = parseFloat(userInput);
+
+if(!isNaN(studentInput) && studentInput >= 0 && studentInput <= 100){
+    const grade = generateGrade(studentInput);
+    console.log("grade",grade);
+}else{
+    console.log("Incorrect input.Value should be between 0 and 100")
+}
+rl.close();
+});
